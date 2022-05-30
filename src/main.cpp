1ac525492bfb716eb3a59e930102cb06a0de927c
mayank arora <52942651+mayankarora450@users.noreply.github.com>
2022-05-30 08:24:06 +0530

    demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,16 @@
 #include <Arduino.h>
-class car
-{
-  char color[10];
-
-public:
-  int speed;
-
-public:
-  void getdata(int distance, int fuel)
-  {
-    Serial.println("the car has covered " + String(distance) + " km and has consumed " + String(fuel) + " L");
-  }
-  void milage(float distance, float fuel);
-  void carspeed()
-  {
-    Serial.println("car speed is " + String(speed));
+class constructor{
+  public:
+  constructor(){
+    Serial.print("hello from constructor");
   }
 };
-
-void car::milage(float d,float f){
-  float carmilage=d/f;
-  Serial.println("car milage is "+String(carmilage));
-}
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  car swift;
-  swift.getdata(120,7);
-  swift.milage(120,7);
-  swift.speed=50;
-  swift.carspeed();
+  constructor obj;
+
 }
 
 void loop()
