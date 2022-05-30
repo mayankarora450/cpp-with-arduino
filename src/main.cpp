3f4e8dd11dc6b5b42f1346fd5640cabbfaddd0f2
mayank arora <52942651+mayankarora450@users.noreply.github.com>
2022-05-30 08:30:05 +0530

    demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,36 @@
 #include <Arduino.h>
-class constructor{
-  public:
-  constructor(){
-    Serial.print("hello from constructor");
+/* When does the destructor get called?
+A destructor is automatically called when:
+1) The program finished execution.
+2) When a scope (the { } parenthesis) containing local variable ends.
+*/
+class destructor
+{
+public:
+  destructor()
+  {
+    Serial.println("hello from constructor");
+  }
+  ~destructor()
+  {
+    Serial.println("hello from destructor");
+  }
+  void display()
+  {
+    Serial.println("hello");
+  }
+  void display2()
+  {
+    Serial.println("hello2");
   }
 };
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  constructor obj;
-
+  destructor obj;
+  obj.display();
+  obj.display2();
 }
 
 void loop()
