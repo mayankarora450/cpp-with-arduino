88540e6bd2e6edc60d830851739133ba4f2d7bb1
mayank arora <52942651+mayankarora450@users.noreply.github.com>
2022-05-31 08:29:57 +0530

    demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,47 @@
 #include <Arduino.h>
+// this is example of compile time polymorphism
+// class A{
+//   public:
+//   int sum(int num1,int num2){
+//     return num1+num2;
+//   }
+//   int sum(int num1,int num2,int num3){
+//     return num1+num2+num3;
+//   }
+// };
+// void setup()
+// {
+//   // put your setup code here, to run once:
+//   Serial.begin(9600);
+//   A obj;
+//   Serial.println(obj.sum(10,20,30));
+//   Serial.println(obj.sum(10,20));
 
-class A
-{
+// }
+
+// this is example of run time polymorphism
+
+class A{
   public:
-  A(){
-    Serial.println("A constructor called");
+  void display(){
+    Serial.println("parent class");
   }
-int var=5;
 };
-
 class B:public A{
   public:
-  B(){
-    Serial.println("B constructor called");
+  void display(){
+    Serial.println("child class");
   }
-  int var2=10;
 };
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-B obj;
-Serial.println(obj.var2);
+ A object1;
+ object1.display();
+ B object2;
+ object2.display();
+
 }
 
 void loop()
